264a0f8f0b684e1012f439e841803740a73a3724
YumaMasterpiece <Gh91@protonmail.com>
2022-01-25 17:28:50 +0100

    feat: Create mapper for Google Book Api

--- Bookstore-googleApi/src/main/java/com/example/bookstore/model/GoogleApiBookDTO.java
+++ Bookstore-googleApi/src/main/java/com/example/bookstore/model/GoogleApiBookDTO.java
@@ -1,20 +1,23 @@
 package com.example.bookstore.model;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.Setter;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.*;
+
+import java.util.List;
 
 @Getter
 @Setter
+@ToString
+@NoArgsConstructor
 @AllArgsConstructor
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class GoogleApiBookDTO {
-    private String author;
+    private List<String> authors;
+    private List<String> categories;
     private String title;
-    private String genre;
     private String description;
     private String publisher;
-    private String ISBN;
-    private String releaseLanguage;
-    private int numberOfPages;
-    private int releaseNumber;
+    private String contentVersion;
+    private String language;
+    private int pageCount;
 }
--- Bookstore-googleApi/src/main/java/com/example/bookstore/service/GoogleApiBookService.java
+++ Bookstore-googleApi/src/main/java/com/example/bookstore/service/GoogleApiBookService.java
@@ -1,24 +1,52 @@
 package com.example.bookstore.service;
 
 import com.example.bookstore.model.GoogleApiBookDTO;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.json.JSONArray;
+import org.json.JSONObject;
 import org.springframework.stereotype.Service;
 
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.List;
+import java.util.Scanner;
 
 @Service
 public class GoogleApiBookService {
+
     public List<GoogleApiBookDTO> getAllBooks() {
         try {
-            URL url = new URL("https://www.googleapis.com/books/v1/volumes?q=flowers&orderBy=newest&key=");
+
+            URL url = new URL("https://www.googleapis.com/books/v1/volumes?q=flowers&orderBy=newest&key="+System.getenv("googleBookApi"));
             HttpURLConnection conn = (HttpURLConnection) url.openConnection();
             conn.setRequestMethod("GET");
             conn.connect();
 
+            if (conn.getResponseCode() != 200) {
+                throw new RuntimeException("HttpResponseCode: " + conn.getResponseCode());
+            } else {
+                StringBuilder jsonInLine = new StringBuilder();
+                Scanner scanner = new Scanner(url.openStream());
+                while (scanner.hasNext()) {
+                    jsonInLine.append(scanner.nextLine());
+                }
+                scanner.close();
+                JSONObject response = new JSONObject(jsonInLine.toString());
+                JSONArray allItems = response.getJSONArray("items");
+                JSONObject volumeInfo = allItems.getJSONObject(0).getJSONObject("volumeInfo");
+                ObjectMapper mapper = new ObjectMapper();
+                GoogleApiBookDTO googleApiBookDTO = mapper.readValue(volumeInfo.toString(), GoogleApiBookDTO.class);
+                System.out.println(googleApiBookDTO.toString());
+            }
+
         } catch (Exception e) {
-            System.out.println(e);
+            e.printStackTrace();
         }
+
         return null;
+    }
+
+    public void connectToGoogleApi(){
+
     }
 }
